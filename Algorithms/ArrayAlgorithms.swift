3eb16959535a7f7d4797569bb2d84ce2f22cd51f
KalaivaniVelusamy <kalai.v.vani@gmail.com>
2021-10-17 22:42:11 +0530

    new algos for array

---
+++ Algorithms/ArrayAlgorithms.swift
@@ -0,0 +1,21 @@
+//
+//  ArrayAlgorithms.swift
+//  Algorithms
+//
+//
+
+import Foundation
+
+class ArrayAlgorithms
+{
+    
+    func duplicateZeros(_ arr: inout [Int]) {
+        
+        var temp = 0
+        
+        while temp < arr.count {
+            arr.sh
+        }
+    }
+
+}
